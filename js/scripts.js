116aa02754bfa7f0ffcffea236ea0d65f69fe2a2
lukasz341 <lukasz341@tlen.pl>
2017-09-07 21:09:08 +0200

    poprawa wyswietlania animacji

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 $(function(){
 	//this code will execute after the DOM is loaded
 	
-});
-
 var carouselList = $("#carousel ul");
 
 function changeSlide() {
@@ -13,9 +11,8 @@ function moveFirstSlide() {
 	var lastItem = carouselList.find("li:last");
 	
 	lastItem.after(firstItem);
-	
 	carouselList.css({marginLeft:0});
 }
+setInterval(changeSlide, 4000);	
+});
 
-setTimeout(changeSlide, 4000);
-setInterval(changeSlide, 4000);
